85173e21085d54c6e6c840e49791e3a63028172e
Tejas Srinivasan <tejas.srinivasan95@gmail.com>
2023-10-22 23:41:25 -0700

    Abrar workshop paper

---
+++ publications.js
@@ -0,0 +1,103 @@
+var papers = [
+    {
+        "paper_title": "Exploring Strategies for Efficient Real-World VLN Evaluation",
+        "authors": "<b>Abrar Anwar</b>, <b>Rohan Gupta</b>, <b>Elle Szabo</b>, <b>Jesse Thomason</b>",
+        "venue": "LangRob Workshop, CoRL 2023",
+        "link": '<a href=\"https://openreview.net/forum?id=uABEHp6tjy\">Paper</a>', 
+        "year": "2023",
+    },
+    {
+        "paper_title": "Chain-of-Questions Training with Latent Answers for Robust Multistep Question Answering",
+        "authors": "<b>Wang Zhu</b>, <b>Jesse Thomason</b>, Robin Jia",
+        "venue": "EMNLP 2023",
+        "link": '<a href=\"https://arxiv.org/abs/2305.14901\">Paper</a>', 
+        "year": "2023",
+    },
+    {
+        "paper_title": "Exploring Strategies for Modeling Sign Language Phonology",
+        "authors": "<b>Lee Kezar</b>, <b>Riley Carlin</b>, <b>Tejas Srinivasan</b>, Zed Sehyr, Naomi Caselli, <b>Jesse Thomason</b>",
+        "venue": "ESANN 2023",
+        "link": '<a href=\"https://www.esann.org/sites/default/files/proceedings/2023/ES2023-83.pdf\">Paper</a>', 
+        "year": "2023",
+    },
+    {
+        "paper_title": "Self-Supervised 3D Representation Learning for Robotics",
+        "authors": "<b>Ishika Singh</b>, <b>Anthony Liang</b>, Mohit Shridhar, <b>Jesse Thomason</b>",
+        "venue": "Pretraining for Robotics Workshop, ICRA 2023",
+        "link": '<a href=\"https://openreview.net/pdf?id=Hqb3t4Jqrk\">Paper</a>', 
+        "year": "2023",
+    },
+    {
+        "paper_title": "ViSaRL: Visual Reinforcement Learning Guided by Human Saliency",
+        "authors": "<b>Anthony Liang</b>, <b>Jesse Thomason</b>, Erdem Bıyık",
+        "venue": "Pretraining for Robotics Workshop, ICRA 2023",
+        "link": '<a href=\"https://openreview.net/pdf?id=1BrsVITE4PD\">Paper</a> / <a href=\"https://sites.google.com/view/visarl\">Website</a>', 
+        "year": "2023",
+    },
+    {
+        "paper_title": "I2I: Initializing Adapters with Improvised Knowledge",
+        "authors": "<b>Tejas Srinivasan</b>, <b>Furong Jia</b>, Mohammad Rostami, <b>Jesse Thomason</b>",
+        "venue": "CoLLAs 2023",
+        "link": '<a href=\"https://arxiv.org/abs/2304.02168\">Paper</a>', 
+        "year": "2023",
+    },
+    {
+        "paper_title": "Multimodal Speech Recognition for Language-Guided Embodied Agents",
+        "authors": "<b>Allen Chang</b>, Xiaoyuan Zhu, Aarav Monga, Seoho Ahn, <b>Tejas Srinivasan</b>, <b>Jesse Thomason</b>",
+        "venue": "Interspeech 2023",
+        "link": '<a href=\"https://arxiv.org/abs/2302.14030\">Paper</a>', 
+        "year": "2023",
+    },
+    {
+        "paper_title": "Curriculum Learning for Data-Efficient Vision-Language Alignment",
+        "authors": "<b>Tejas Srinivasan</b>, Xiang Ren, <b>Jesse Thomason</b>",
+        "venue": "Workshop on Open-Domain Reasoning Under Multi-Modal Settings (O-DRUM), CVPR 2023",
+        "link": '<a href=\"https://arxiv.org/abs/2207.14525\">Paper</a>', 
+        "year": "2023",
+    },
+    {
+        "paper_title": "Iterative Vision-and-Language Navigation",
+        "authors": "Jacob Krantz, Shurjo Banerjee, <b>Wang Zhu</b>, Jason Corso, Peter Anderson, Stefan Lee, <b>Jesse Thomason</b>",
+        "venue": "CVPR 2023",
+        "link": '<a href=\"https://arxiv.org/abs/2210.03087\">Paper</a> / <a href=\"https://jacobkrantz.github.io/ivln\">Website</a> / <a href=\"https://github.com/Bill1235813/IVLN\">Code</a>', 
+        "year": "2023",
+    },
+    {
+        "paper_title": "Improving Sign Recognition with Phonology",
+        "authors": "<b>Lee Kezar</b>, <b>Jesse Thomason</b>, Zed Sevcikova Sehyr",
+        "venue": "EACL 2023",
+        "link": '<a href=\"https://arxiv.org/abs/2302.05759\">Paper</a>', 
+        "year": "2023",
+    },
+    {
+        "paper_title": "Transformer Adapters for Robot Learning",
+        "authors": "<b>Anthony Liang</b>, <b>Ishika Singh</b>, Karl Pertsch, <b>Jesse Thomason</b>",
+        "venue": "Pretraining for Robot Learning Workshop, CoRL 2022",
+        "link": '<a href=\"https://openreview.net/forum?id=H--wvRYBmF\">Paper</a>', 
+        "year": "2022",
+    },
+    {
+        "paper_title": "Generalization Differences between End-to-End and Neuro-Symbolic Vision-Language Reasoning Systems",
+        "authors": "<b>Wang Zhu</b>, <b>Jesse Thomason</b>, Robin Jia",
+        "venue": "EMNLP 2022",
+        "link": '<a href=\"https://arxiv.org/abs/2210.15037\">Paper</a>', 
+        "year": "2022",
+    },
+    {
+        "paper_title": "ProgPrompt: Generating Situated Robot Task Plans using Large Language Models",
+        "authors": "<b>Ishika Singh</b>, Valts Blukis, Arsalan Mousavian, Ankit Goyal, Danfei Xu, Jonathan Tremblay, Dieter Fox, <b>Jesse Thomason</b>, Animesh Garg",
+        "venue": "ICRA 2023",
+        "link": '<a href=\"https://arxiv.org/abs/2209.11302\">Paper</a> / <a href=\"https://progprompt.github.io/\">Website</a> / <a href=\"https://www.youtube.com/watch?v=cawHI_rYQoM\">Video</a>', 
+        "year": "2023",
+    },
+    {
+        "paper_title": "CLiMB: A Continual Learning Benchmark for Vision-and-Language Tasks",
+        "authors": "<b>Tejas Srinivasan, Ting-Yun Chang, Leticia Pinto Alva</b>, Georgios Chochlakis, Mohammad Rostami, <b>Jesse Thomason</b>",
+        "venue": "NeurIPS 2022 Datasets and Benchmarks",
+        "link": '<a href=\"https://arxiv.org/abs/2206.09059\">Paper</a> / <a href=\"https://github.com/GLAMOR-USC/CLiMB\">Code</a>', 
+        "year": "2022",
+    },
+
+]
+
+
